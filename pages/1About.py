73ac382ffc78d0a52a46521d8f26703133b875c2
ashok.office <ashokguntu@outlook.com>
2023-07-04 19:03:42 +0530

    c

---
+++ pages/1About.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Stream Discharge",
+    page_icon="🚧",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+st.info('This page explains the underlying logic and computations powering this application and is still under development')
---
+++ pages/2Contact.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Stream Discharge",
+    page_icon="🚧",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
